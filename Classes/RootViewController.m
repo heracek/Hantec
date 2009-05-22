94c5e8db9f5ee5776a1cf8a5835096991dc7ffac
Tomas Horacek <tomas.horacek@gmail.com>
2009-05-22 16:19:04 +0200

    Spravne osetren inset tabulek pri zobrazeni a schovani klavesnice.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -26,6 +26,16 @@
 	_searchBar.autocorrectionType = UITextAutocorrectionTypeNo;
 	_searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
 	_searchBar.showsCancelButton = NO;
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(keyboardWillShow:)
+												 name:UIKeyboardWillShowNotification
+											   object:nil];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(keyboardWillHide:)
+												 name:UIKeyboardWillHideNotification
+											   object:nil];
 }
 
 // Override to allow orientations other than the default portrait orientation.
@@ -164,28 +174,6 @@
     [super dealloc];
 }
 
-- (void)hideIndexAndMoveTableUp:(BOOL)hide {
-	CGRect rect = self.tableView.frame;
-	rect.size.height += (hide) ? -167 : +167;
-	
-	UITableView *tableView = self.tableView;
-	//[UIView beginAnimations:nil context:nil];
-	
-	
-	
-	NSObject <NonPublicTableViewMethods> *noCompilerWarningTableView \
-		= (NSObject <NonPublicTableViewMethods> *)tableView;
-	[noCompilerWarningTableView setIndexHidden:hide animated:YES];
-	
-	
-	
-	//[UIView commitAnimations];
-	[UIView beginAnimations:nil context:@"hideIndexAndMoveTableUp"];
-	[UIView setAnimationDuration:(hide) ? 2.7 : 0.3];
-	self.tableView.frame = rect;
-	[UIView commitAnimations];
-}
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 	[_detailsController setDictionaryItem:[self getDictionaryItemForIndexPath:indexPath]
 					   isFromOriginalDict:_isFromOriginalDict];
@@ -197,12 +185,10 @@
 
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
 	_searchBar.showsCancelButton = YES;
-	[self hideIndexAndMoveTableUp:YES];
 }
 
 - (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
 	_searchBar.showsCancelButton = NO;
-	[self hideIndexAndMoveTableUp:NO];
 }
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
@@ -273,6 +259,51 @@
 	}
 	
 	return wordOfTheDay;
+}
+
+#pragma mark -
+#pragma mark Keyboard Notifications
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    CGRect bounds = [[[notification userInfo] objectForKey:UIKeyboardBoundsUserInfoKey] CGRectValue];
+    CGPoint center = [[[notification userInfo] objectForKey:UIKeyboardCenterEndUserInfoKey] CGPointValue];
+    
+    // We need to compute the keyboard and table view frames in window-relative coordinates
+    CGRect keyboardFrame = CGRectMake(round(center.x - bounds.size.width/2.0), round(center.y - bounds.size.height/2.0), bounds.size.width, bounds.size.height);
+    CGRect tableViewFrame = [self.tableView.window convertRect:self.tableView.frame fromView:self.tableView.superview];
+    
+    // And then figure out where they overlap
+    CGRect intersectionFrame = CGRectIntersection(tableViewFrame, keyboardFrame);
+    
+    // This assumes that no one else cares about the table view's insets...
+    UIEdgeInsets insets = UIEdgeInsetsMake(0, 0, intersectionFrame.size.height, 0);
+	
+	[UIView beginAnimations:nil context:nil];
+    
+	[self.tableView setContentInset:insets];
+    [self.tableView setScrollIndicatorInsets:insets];
+	
+	[UIView commitAnimations];
+	
+	NSObject <NonPublicTableViewMethods> *noCompilerWarningTableView \
+		= (NSObject <NonPublicTableViewMethods> *)self.tableView;
+	[noCompilerWarningTableView setIndexHidden:YES animated:NO];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+	NSObject <NonPublicTableViewMethods> *noCompilerWarningTableView \
+		= (NSObject <NonPublicTableViewMethods> *)self.tableView;
+	[noCompilerWarningTableView setIndexHidden:NO animated:NO];
+	
+	[UIView beginAnimations:nil context:nil];
+    
+	// This assumes that no one else cares about the table view's insets...
+    [self.tableView setContentInset:UIEdgeInsetsZero];
+    [self.tableView setScrollIndicatorInsets:UIEdgeInsetsZero];
+	
+	[UIView commitAnimations];
 }
 
 @end
